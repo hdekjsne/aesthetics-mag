ce073754e7f16ac3042913aa7bf49c8774ea7558
Анастасия Мамаева <hdekjsne@gmail.com>
2024-01-05 13:58:17 +0300

    server again

--- server.js
+++ server.js
@@ -1,20 +1,15 @@
 export const server = Bun.serve({
 	async fetch(req) {
+		if (server.upgrade(req)) {
+			return;
+		}
 		if (/js$/.test(req.url)) {
-			console.log(req.url);
 			const file = await Bun.file('./dist/index.js');
-			if (server.upgrade(req)) {
-				return;
-			}
 			return new Response(file, {
 				headers: { 'Content-Type': 'text/javascript' }
 			});
 		} else {
 			const file = await Bun.file('./index.html');
-			if (server.upgrade(req)) {
-				console.log('upgraded ws');
-				return;
-			}
 			return new Response(file, {
 				headers: { 'Content-Type': 'text/html' }
 			});	
