fb9da5c44ec4cda4230f6a9eb9ea013561370820
Анастасия Мамаева <hdekjsne@gmail.com>
2024-01-06 18:42:42 +0300

    small changes at compiler

--- compileSass.js
+++ compileSass.js
@@ -1,5 +1,5 @@
 import * as sass from 'sass';
 
 const compiled = await sass.compileAsync('./src/style.scss');
-//console.log(compiled.css);
+console.log('i\'ve compiled');
 Bun.write('./dist/style.css', compiled.css);
