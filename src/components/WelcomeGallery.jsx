ed87fd79285a355577ea9f474f1490b2337e7a18
Анастасия Мамаева <hdekjsne@gmail.com>
2024-01-08 22:05:25 +0300

    welcome gallery component

---
+++ src/components/WelcomeGallery.jsx
@@ -0,0 +1,3 @@
+export default function WelcomeGallery() {
+	return <section className="main-gallery"></section>;
+}
--- src/components/WelcomePage.jsx
+++ src/components/WelcomePage.jsx
@@ -2,20 +2,22 @@ import { useContext } from "react";
 import { ThemeContext } from "../Context.jsx";
 import ArticlePreview from "./ArticlePreview.jsx";
 import { data } from "../reference.js";
+import WelcomeGallery from "./WelcomeGallery.jsx";
 
 export default function WelcomePage(props) {
 	const context = useContext(ThemeContext);
 	return (
 		<section className={`main main-${context}`}>
 			<div className="main-restricter">
-			<section className="article-preview-container">
-				<ArticlePreview
-					title={data.title} tags={data.tags}
-					authors={data.authors} description={data.description}
-					modeSwitcherRead={props.modeSwitcherRead}>
-				</ArticlePreview>
-			</section>
+				<WelcomeGallery></WelcomeGallery>
+				<section className="article-preview-container">
+					<ArticlePreview
+						title={data.title} tags={data.tags}
+						authors={data.authors} description={data.description}
+						modeSwitcherRead={props.modeSwitcherRead}>
+					</ArticlePreview>
+				</section>
 			</div>
 		</section>
-	)
+	);
 }
