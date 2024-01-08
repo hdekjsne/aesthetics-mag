3d9f3635341b03e6b08b996e7679363647e186fe
Анастасия Мамаева <hdekjsne@gmail.com>
2024-01-08 20:37:29 +0300

    small fix in theme-switcher + gitignore

--- src/components/ThemeSwitcher.jsx
+++ src/components/ThemeSwitcher.jsx
@@ -4,9 +4,8 @@ import { ThemeContext } from "../Context"
 export default function ThemeSwitcher(props) {
 	const context = useContext(ThemeContext);
 	return (
-		<div className="theme-switcher-container">
-			<div className={`theme-switcher theme-switcher-${context}`}
-				onClick={props.themeSwitcher}>
+		<div className="theme-switcher-container" onClick={props.themeSwitcher}>
+			<div className={`theme-switcher theme-switcher-${context}`}>
 				<div></div>
 				<div></div>
 			</div>
