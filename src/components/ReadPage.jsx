56f73fc7293ee609f1e3d66519ae7d583d755d10
Анастасия Мамаева <hdekjsne@gmail.com>
2024-01-10 10:41:09 +0300

    clean ReadPage code

--- src/components/ReadPage.jsx
+++ src/components/ReadPage.jsx
@@ -4,35 +4,9 @@ import { data } from "../reference.js";
 
 export default function ReadPage() {
 	const context = useContext(ThemeContext);
-	//const [state, setState] = useState('load');
-/*
-	const { default: Article } = import(data.jsxPath).then((received) => {
-			setState('show');
-			console.log(received);
-			return received;
-		}).catch(() => setState('error'));
-	let jsx;
-*/
 	const path = import.meta.resolve('../reference.jsx');
 	const Article = lazy(() => import(path));
-/*
-	switch (state) {
-		case 'load':
-			jsx = <p>loading...</p>
-			break;
-		case 'show':
-			console.log(state);
-			console.log(ThemeContext);
-			console.log(Article);
-			jsx = <Article context={context}></Article>;
-			break;
-		case 'error':
-			jsx = <p>error occured!</p>
-			break;
-		default:
-			console.log('even \'error\' state didn\'t help. something is deeply wrong');
-	}
-*/
+
 	return (
 		<section className={`main main-${context}`}>
 			<div className="main-restricter">
