e235710926907b71371d9ebc2b18f2e857fc39a3
Настя <hdekjsne@gmail.com>
2024-03-20 05:47:26 +0300

    change dist to public

--- compileSass.js
+++ compileSass.js
@@ -2,4 +2,4 @@ import * as sass from 'sass';
 
 const compiled = await sass.compileAsync('./src/style.scss');
 console.log('i\'ve compiled');
-Bun.write('./dist/style.css', compiled.css);
+Bun.write('./public/style.css', compiled.css);
